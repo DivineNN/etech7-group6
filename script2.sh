0ea2c59eaf9c31750e5d6650f17b4aa1626fdd36
DivineNN <ngantchadi@yahoo.com>
2023-09-18 14:01:01 -0400

    adding another command to mount ebs vol

--- script2.sh
+++ script2.sh
@@ -1,4 +1,6 @@
 how are you
 #this is just for demo
 #echo "this is git on visual studio code"
+#kindly review again
+what is your name
 
---
+++ test2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This is to demo shell scripting
+echo -n "Please enter your name: "
+read name
+echo -n "Please enter your team: "
+read team
+echo "OHH Nice,Welcome to Etech Devops master class $name and you are also welcome to $team"
+echo "End of script"
